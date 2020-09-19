a0f77bb8cb5155639a43aa4c74aa648d872fdf58
fedrfedra <vetervell@gmail.com>
2020-09-20 00:10:37 +0300

    added rest and eureka

--- src/main/java/com/example/kgs/controllers/ShortLinkController.java
+++ src/main/java/com/example/kgs/controllers/ShortLinkController.java
@@ -1,20 +1,32 @@
 package com.example.kgs.controllers;
 
 
+import com.example.kgs.services.ShortLinkGenerator;
 import com.example.kgs.services.ShortLinkGetter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/api/")
 public class ShortLinkController {
+
+    @Autowired
+    ShortLinkGenerator generator;
 
     @Autowired
     ShortLinkGetter shortLinkGetter;
 
-    @GetMapping("/get_short_link")
-    public String getShortLink(){
-        return shortLinkGetter.getShortLink().toString();
+    @GetMapping("get_short_link")
+    public String getShortLink() {
+        return shortLinkGetter.getShortLink();
+    }
+
+    @GetMapping("generate")
+    public String generate() {
+        return generator.generateStrings();
     }
 
 }
